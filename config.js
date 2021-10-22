544a8cdb9966300acc7c81f0b81c823bdf501279
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-22 17:43:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['919002370263','917622836211'] // Put your number here
+global.owner = ['919832361550','917003213983'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
